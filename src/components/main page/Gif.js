7db8ef7025f6b9303771b3b28c6f5a2dfd54a1aa
Pele <pelebahalag@gmail.com>
2022-11-30 10:39:11 +0200

    update

--- src/components/main page/Gif.js
+++ src/components/main page/Gif.js
@@ -1,9 +1,15 @@
 import classes from "./Gif.module.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Gif = (props) => {
   return (
     <div className={classes.screen}>
-      <img className={classes.src} src={props.gif} alt="loading..." />
+      <img
+        className={classes.src}
+        src={props.gif}
+        alt={<Skeleton height={200} baseColor="lightgray" />}
+      />
     </div>
   );
 };
