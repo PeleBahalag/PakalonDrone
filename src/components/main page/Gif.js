fd3ec84b415c71c86ab64e43a26145ef36073c1f
Pele <pelebahalag@gmail.com>
2022-11-30 10:49:33 +0200

    update

--- src/components/main page/Gif.js
+++ src/components/main page/Gif.js
@@ -1,14 +1,22 @@
 import classes from "./Gif.module.css";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { useState } from "react";
 
 const Gif = (props) => {
+  const [loading, setLoading] = useState(true);
+  const handleImageLoaded = () => {
+    setLoading(false);
+  };
+
   return (
     <div className={classes.screen}>
+      {loading && <Skeleton height={200} baseColor="lightgray" />}
       <img
+        onLoad={handleImageLoaded.bind(this)}
         className={classes.src}
         src={props.gif}
-        alt={<Skeleton height={200} baseColor="lightgray" />}
+        alt=""
       />
     </div>
   );
