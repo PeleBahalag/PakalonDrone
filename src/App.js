f4ae17bdc953849db6649888505639ed57604809
Pele <pelebahalag@gmail.com>
2022-11-30 09:25:30 +0200

    sync files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useContext } from "react";
+import Context from "./store/context";
+
+import DropdownBox from "./components/dropdown/DropdownBox";
+import Title from "./components/main page/Title";
+import Search from "./components/search/Search";
+import SectionHandler from "./components/Sections/SectionHandler";
+import Bottombar from "./components/UI/Bottombar";
+import Topbar from "./components/UI/Topbar";
+import Whitespace from "./components/UI/Whitespace";
+import ContextProvider from "./store/ContextProvider";
 
 function App() {
+  const ctx = useContext(Context);
+  const [searchBarOpened, setSearchBarOpened] = useState(false);
+  const [subjectsOpened, setSubjectsBarOpened] = useState(false);
+  const [page, setPage] = useState(ctx.page);
+
+  const openSearch = () => {
+    setSearchBarOpened(true);
+    setSubjectsBarOpened(false);
+  };
+  const closeSearch = () => {
+    setSearchBarOpened(false);
+  };
+
+  const changePage = (p) => {
+    setPage(Number(p));
+  };
+
+  const toggleSubjects = () => {
+    setSubjectsBarOpened(!subjectsOpened);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextProvider>
+      <Whitespace height={"6rem"} />
+      <Topbar
+        openSearch={openSearch}
+        toggleSubs={toggleSubjects}
+        isSubjectsOpened={subjectsOpened}
+        changePage={changePage}
+      />
+      {subjectsOpened && (
+        <DropdownBox closeDD={toggleSubjects} changePage={changePage} />
+      )}
+      {page === 0 && <Title changePage={changePage} />}
+      {searchBarOpened && <Search closeSearch={closeSearch} />}
+      {page !== 0 && <SectionHandler />}
+      <Bottombar />
+    </ContextProvider>
   );
 }
 
