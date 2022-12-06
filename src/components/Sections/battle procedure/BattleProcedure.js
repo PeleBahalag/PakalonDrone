7e38d0f027f0920c07323aedb5e8d81525a71afa
Pele <pelebahalag@gmail.com>
2022-12-06 10:39:49 +0200

    c

--- src/components/Sections/battle procedure/BattleProcedure.js
+++ src/components/Sections/battle procedure/BattleProcedure.js
@@ -1,5 +1,8 @@
 import classes from "./BattleProcedure.module.css";
 
+import { GoPin } from "react-icons/go";
+import { GiStopSign } from "react-icons/gi";
+
 const BattleProcedure = (props) => {
   return (
     <div className={classes.section}>
@@ -9,6 +12,14 @@ const BattleProcedure = (props) => {
         ניתוח ומיפוי שטח הוא השלב הראשון בנוה"ק הנעשה באמצעות לימוד השטח דרך 8
         שלבים:
       </p>
+      <section className={classes.flex}>
+        <div className={classes.item}>
+          <GoPin className={classes.logo} size={55} />
+        </div>
+        <div className={classes.item}>
+          <GiStopSign className={classes.logo} size={55} />
+        </div>
+      </section>
     </div>
   );
 };
